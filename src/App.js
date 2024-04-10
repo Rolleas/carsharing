1b797170dc229d1a733ec8973ec2ea01f4988072
rolles <peronsos@gmail.com>
2024-04-10 23:47:39 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {InfoPage} from "./components/pages/info";
 function App() {
   return (
   <Routes>
-    <Route path="/carsharing" element={<HomePage/>}>
+    <Route path="/carsharing/" element={<HomePage/>}>
         <Route index element={<InfoPage/>}/>
         <Route path="/carsharing/about" element={<InfoPage/>}/>
     </Route>
